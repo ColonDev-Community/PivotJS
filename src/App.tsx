3b38a36f9904bde24a5890c8238ba66806d83a1a
sachithamh <sachithamh@gmail.com>
2022-12-21 14:07:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css'
+import Game from './pivotJS/Game'
 function App() {
+  const game = new Game('canvas') 
+
+
+
+
   return (
     <div className="App">
       <canvas id="canvas">
